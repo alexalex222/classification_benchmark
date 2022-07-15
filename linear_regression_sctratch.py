59fa3e6f2b081620e053cb0769749175876bcac5
Kuilin Chen <alexchen199023@gmail.com>
2022-07-15 12:52:49 -0400

    update

--- linear_regression_sctratch.py
+++ linear_regression_sctratch.py
@@ -74,4 +74,59 @@ plt.show()
 ax = plt.axes(projection='3d')
 ax.plot3D(theta_1_list, theta_2_list, loss_list, 'gray')
 plt.show()
+
+
+# %%
+from torch import nn
+
+# define our model by inheriting a predefined class
+class LinearModel(nn.Module):
+    def __init__(
+        self, 
+        input_dim: int, 
+        output_dim: int, 
+        ):
+        super().__init__()
+        self.linear = nn.Linear(in_features=input_dim, out_features=output_dim, bias=True)
+
+    def forward(self, x):
+        # this is y_hat = f(x)
+        y_hat = self.linear(x)
+        return y_hat
+
+linear_model_pytorch = LinearModel(input_dim=2, output_dim=1)
+
+from torch.optim import SGD
+
+# Use the build-in Stochastic Gradient Descent (SGD) optimizer
+optimizer = SGD(
+    params=linear_model_pytorch.parameters(),   # the parameter you want to optimize
+    lr=1e-4,                                    # learning rate
+    )
+
+criterion = nn.MSELoss(reduction='sum')
+
+linear_model_pytorch.train()
+
+# define the training loop
+
+for epoch in range(200):
+
+     # forward pass
+     pred_y = linear_model_pytorch(x_train)
+
+     # compute the loss
+     loss = criterion(pred_y.reshape(-1), y_train)
+
+     # backward pass to compute the gradient
+     loss.backward()
+
+     # update the model parameters using the gradient
+     optimizer.step()
+
+     # zero the parameter gradients
+     optimizer.zero_grad()
+
+print(linear_model_pytorch.linear.weight.data)
+print(linear_model_pytorch.linear.bias.data)
 # %%
